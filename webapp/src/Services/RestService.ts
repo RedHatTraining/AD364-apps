da724e76b25de0d38bc29623be77a1065ea0620f
Marek <mczernek@redhat.com>
2021-01-14 14:04:24 +0100

    revert incorrect creds

--- webapp/src/Services/RestService.ts
+++ webapp/src/Services/RestService.ts
@@ -11,8 +11,8 @@ export abstract class RestService {
         this.axiosInstance = Axios.create({
             baseURL,
             auth: {
-                username: "admin",
-                password: "admin",
+                username: "kieserver",
+                password: "kieserver1!",
             },
         });
         this.axiosInstance.defaults.headers.post["Content-Type"] = "application/json";
